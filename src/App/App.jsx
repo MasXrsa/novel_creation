05575012a002c4bdeaa418748e363fe17d910be8
MasXrsa <twinkmaximlove510@gmail.com>
2024-08-08 21:55:36 +0300

    change files stucture

---
+++ src/App/App.jsx
@@ -0,0 +1,33 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <main className="App">
+    </main>
+  );
+}
+
+export default App;
+
+// import React from "react";
+// import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Dishes from "./components/Dishes";
+// import About from "./components/About";
+// import Mission from "./components/Mission";
+// import Expertise from "./components/Expertise";
+
+// const App = () => {
+//   return (
+//     <main className="overflow-y-hidden text-neutral-200 antialiased">
+//       <Hero />
+//       <Navbar />
+//       <Dishes />
+//       <About />
+//       <Mission />
+//       <Expertise />
+//     </main>
+//   )
+// }
+
+// export default App;
---
+++ src/Constants/index.jsx
@@ -0,0 +1,21 @@
+import commander from "../assets/icon/commander.png";
+import dev from "../assets/icon/dev.png";
+
+export const MAIN = [
+    {
+      image: dev,
+      title: "Spaghetti Carbonara",
+      description: "Creamy pasta with bacon and cheese",
+    },
+    {
+      image: commander,
+      title: "Chicken Tikka Masala",
+      description: "Indian curry with tender chicken in spicy sauce",
+    },
+    {
+      image: dish10,
+      title: "Tiramisu",
+      description:
+        "Italian dessert with layers of coffee-soaked biscuits and mascarpone",
+    }
+  ];
